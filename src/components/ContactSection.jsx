5a1ae07574b90065b42f3de91dee7893314ac284
Sakshi Deshmukh <d1sakshi@gmail.com>
2026-03-10 01:09:20 +0530

    Add contact icons

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,12 +1,19 @@
+import {FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 function ContactSection(){
     return(
         <section id="contact">
             <h2>Contact</h2>
             <p>If you'd like to work together or have any questions,feel free to reach out.</p>
             <div className="contact-links">
-                <a href="mailto:d1sakshi@gmail.com">Email</a>
-                <a href="https://github.com/sakshideshmukh-ai">Github</a>
-                <a href="https://linkedin.com/in/sakshi-fullstack">LinkedIn</a>
+                <a href="mailto:d1sakshi@gmail.com">
+                    <FaEnvelope/>Email
+                    </a>
+                <a href="https://github.com/sakshideshmukh-ai">
+                <FaGithub/>Github
+                </a>
+                <a href="https://linkedin.com/in/sakshi-fullstack">
+                <FaLinkedin/>LinkedIn
+                </a>
             </div>
         </section>
     
